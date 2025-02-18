6227a80c01f5dcbbd8fbcfc7e882d64fdcdfff48
ronan-morris <78508701+ronan-morris@users.noreply.github.com>
2025-02-18 13:09:10 -0500

    Update and rename program_many.bat to program_many_safe.bat

--- program_many_safe.bat
+++ program_many_safe.bat
@@ -13,7 +13,7 @@
 
 
 :START
-%QUARTUS_BIN%\quartus_pgm.exe -m jtag -c 1 -o "p;DE10_LITE_Default.pof"
+%QUARTUS_BIN%\quartus_pgm.exe -m jtag -c 1 -o "p;safe.pof"
 @ set SOPC_BUILDER_PATH=%SOPC_KIT_NIOS2%+%SOPC_BUILDER_PATH%
 
 
